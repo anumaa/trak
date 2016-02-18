db2a5afcbf7176f735105ba9398af369e9a36c2d
Anu M <narnua@gmail.com>
2016-02-18 23:46:04 +0200

    Some progress

--- gui.py
+++ gui.py
@@ -2,14 +2,13 @@
 
 
 from tkinter import *
+from tkinter import ttk
 
 from project import * 
 from task import * 
 
 
 class GUI(Frame):
-
-
 
 
 	def __init__(self, parent, tasks):
@@ -19,13 +18,27 @@ class GUI(Frame):
 		self.initUI()
 		self.previousTask = -1
     
-    
-	def start(self):
-		
+
+	def foo(self, event):
+		current = self.taskList.current()
+		print(self.tasks[current].getName())
+
+
+	def start(self, event):
+
+		print("current: " + str(self.taskList.current()))
+		whichTask = int(self.taskList.current())
+
+		print(self.tasks[whichTask].getName())
+		print(whichTask)
+		t = self.tasks[whichTask]
+
+
+
 		#print "previous: " + str(self.previousTask)
 		if(self.previousTask != -1): 
 			self.stop()
-		whichTask = self.newTask.get()
+		#whichTask = self.newTask.get()
 		#print "which: " + str(whichTask)
 		
 		task = self.tasks[whichTask]
@@ -50,9 +63,106 @@ class GUI(Frame):
 				print ("\tSESSION " + str(i) + ": " + str(s.getTotalTime()))
 				#print time.strftime('%H:%M:%S', time.localtime(s.getTotalTime()))
 				i = i + 1
+
+
+
+	def editList(self):
+
+		#textWin = Toplevel()
+
+		#textarea with existing items
+		print('')
+
+		self.taskListText = Text(self, height=10, width=30)
+		for t in self.tasks:
+			self.taskListText.insert(END, t.getName()+'\n')
+		self.taskListText.pack(side=BOTTOM)
+
+		taskListButton = Button(self, command = self.updateList, text='Update')
+		taskListButton.pack(side=BOTTOM)
+
+		self.startButton = Button(self, justify = LEFT, command = self.start, image=self.playImg)
+		#self.startButton.place(x=50, y=0)
+
+
 	
-	
-	
+	def visualize(self):
+		# create child window
+		visuWin = Toplevel()
+		# display message
+		message = "This is the child window"
+		Label(visuWin, text=message).pack()
+		# quit child window and return to root window
+		# the button is optional here, simply use the corner x of the child window
+		Button(visuWin, text='OK', command=visuWin.destroy).pack()
+
+		vis = Canvas(visuWin, width=400, height=250)
+		vis.pack()
+
+
+
+		#vis.create_rectangle(50, 20, 150, 80, fill="#476042")
+		##w.create_rectangle(65, 35, 135, 65, fill="yellow")
+		#w.create_line(0, 0, 50, 20, fill="#476042", width=3)
+		#w.create_line(0, 100, 50, 80, fill="#476042", width=3)
+		#w.create_line(150,20, 200, 0, fill="#476042", width=3)
+		#w.create_line(150, 80, 200, 100, fill="#476042", width=3)
+
+		x0 = 0
+		y0 = 0
+		h = 30
+		w = 0
+
+		allTotal = 0
+		for t in self.tasks:
+			allTotal = allTotal + t.getTotalTime()
+
+		for t in self.tasks:
+
+			tot = t.getTotalTime()
+
+
+			for s in t.getSessions():
+
+				if(tot != 0):
+					w = (s.getTotalTime()*1.0 / allTotal)*150
+
+				print("\ns.tot: " + str(s.getTotalTime()))
+				print("w: " + str(w))
+				print("tot: " + str(tot))
+				vis.create_rectangle(x0, y0, x0+w, y0+h, fill="green")
+				x0 = x0+w
+
+			x0 = 0
+			y0 = y0 + h + 10
+
+				#vis.create_rectangle(10,10,150,150,fill='black')
+
+
+
+	def updateList(self):
+		print('updated list')
+
+		allTasks = self.taskListText.get("1.0",END)
+		updatedTasks = allTasks.split('\n')
+
+
+		for t in self.tasks:
+			if t.getName() not in updatedTasks:
+				t.setStatus('archived')
+
+		for ut in updatedTasks:
+			if ut not in self.tasks():
+				print('')
+
+
+
+
+		print(updatedTasks)
+
+
+
+
 	def initUI(self):
 
 		self.parent.title("trak v0.1")
@@ -60,23 +170,64 @@ class GUI(Frame):
 		self.pack(fill=BOTH, expand=True)
 		self.var = BooleanVar()
 
-		startButton = Button(self, text="START", command = self.start)
-		startButton.place(x=50, y=50)
+		taskNames = []
+		for t in self.tasks:
+			taskNames.append(t.getName())
 
-		stopButton = Button(self, text="STOP", command = self.stop)
-		stopButton.place(x=150, y=50)
+
+
+
+		self.newTask = StringVar()
+		self.taskList = ttk.Combobox(self, textvariable=self.newTask, values=taskNames)
+		#self.taskList['values'] = taskNames
+		self.taskList.bind('<<ComboboxSelected>>',self.start)
+		#self.taskList.place(x='0', y='0')
+		self.taskList.current(0)
+		self.taskList.pack(side=LEFT)
+
+		print(self.newTask)
+
+
+		self.playImg = PhotoImage(file='images/play.png')
+		self.startButton = Button(self, justify = LEFT, command = self.start, image=self.playImg)
+		#self.startButton.place(x=50, y=0)
+		self.startButton.pack(side=LEFT)
+
+		self.pauseImg = PhotoImage(file='images/pause.png')
+		self.stopButton = Button(self, justify = LEFT, command = self.stop, image=self.pauseImg)
+		#self.stopButton.place(x=86, y=0)
+		self.stopButton.pack(side=LEFT)
+
+		self.ejectImg = PhotoImage(file='images/eject.png')
+		self.editButton = Button(self, justify = LEFT, command=self.editList, image=self.ejectImg)
+		#self.editButton.place(x=122, y=0)
+		self.editButton.pack(side=LEFT)
+
+
+
+		self.visuButton = Button(self, text="VISU", command = self.visualize)
+		self.visuButton.place(x=150, y = 90)
 		
 		
-		printButton = Button(self, text="PRINT", command = self.printAll)
-		printButton.place(x=150, y=70)
+		self.printButton = Button(self, text="PRINT", command = self.printAll)
+		self.printButton.place(x=500	, y=300)
 
-		self.newTask = IntVar()
-		i = 0
-		for t in self.tasks: 
-			print (t.getName())
-			Radiobutton(self, text='Task '+str(i), value=i,  variable=self.newTask, indicatoron=0, command=self.start).pack(anchor=W)
-			i = i+1
+		#self.newTask = IntVar()
+		#i = 0
+		#for t in self.tasks:
+		#	print (t.getName())
+		#	Radiobutton(self, text='Task '+str(i), value=i,  variable=self.newTask, indicatoron=0, command=self.start).pack(anchor=W)
+		#	i = i+1
 		        #t.getName()
+
+
+
+
+		#http://pyinmyeye.blogspot.fi/2012/08/tkinter-combobox-demo.html
+		#todo add to list
+		#cb1 = ttk.Combobox(self)
+		#cb1.bind('<Return>', self.updateList)
+		#cb1.pack(pady=5, padx=10)
         
 		#Radiobutton(self, text="One", value=1).pack(anchor=W)
 		#Radiobutton(self, text="Two", value=2).pack(anchor=W)
@@ -90,7 +241,8 @@ class GUI(Frame):
 		#    variable=self.var, command=self.onClick)
 		#cb.select()
 		#cb.place(x=50, y=50)
-        
+
+
 
 
 
@@ -105,7 +257,7 @@ def main():
 
 
 	root = Tk()
-	root.geometry("250x150+300+300")
+	#root.geometry("250x150+300+300")
 	app = GUI(root, tasks)
     
 
