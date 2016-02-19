a4b1b5d5064b41c26d880f9846b7669a483ad67e
Anu M <narnua@gmail.com>
2016-02-19 12:56:07 +0200

    visu open

--- trak.py
+++ trak.py
@@ -21,6 +21,7 @@ class GUI(Frame):
 		self.initUI()
 		self.previousTask = -1
 		self.editListVisible = FALSE
+		self.visuVisible = FALSE
 		#self.wm_protocol("WM_DELETE_WINDOW", self.onExit)
 
 
@@ -33,19 +34,19 @@ class GUI(Frame):
 		#pickle
 		self.destroy()
 
-	def start(self, event):
 
-		print("current: " + str(self.taskList.current()))
-		self.stopButton.config(relief=RAISED)
-		self.startButton.config(relief=SUNKEN)
+	def start1(self, event):
+		self.start()
+
+
+	def start(self):
+
+		print("self.start, current: " + str(self.taskList.current()))
 		whichTask = int(self.taskList.current())
 
 		print(self.tasks[whichTask].getName())
 		print(whichTask)
 		t = self.tasks[whichTask]
-
-
-
 
 		#print "previous: " + str(self.previousTask)
 		if(self.previousTask != -1): 
@@ -59,10 +60,13 @@ class GUI(Frame):
 		print ('START ' + str(task.__str__('red')) + "SESSION " + str(len(self.tasks[whichTask].getSessions())+1) + " " + taskTime)  
 		self.tasks[whichTask].startSession()
 		self.previousTask = whichTask
-		
-		
+
+		self.stopButton.config(relief=RAISED)
+		self.startButton.config(relief=SUNKEN)
+
 	
 	def stop(self):
+		print('self.stop()')
 		self.tasks[self.previousTask].endSession()
 		self.stopButton.config(relief=SUNKEN)
 		self.startButton.config(relief=RAISED)
@@ -102,6 +106,11 @@ class GUI(Frame):
 
 	
 	def visualize(self):
+
+		maxWidth = self.winfo_width()
+
+
+		if not self.visuVisible:
 		# create child window
 		#visuWin = Toplevel()
 		# display message
@@ -111,48 +120,46 @@ class GUI(Frame):
 		# the button is optional here, simply use the corner x of the child window
 		#Button(self, text='OK', command=visuWin.destroy).pack()
 
-		vis = Canvas(self, width=400, height=250)
-		#vis.pack()
-		vis.grid(row=2,column=0)
+			self.visuVisible = TRUE
+			self.vis = Canvas(self, width=maxWidth, height=400)
+			#vis.pack()
+			self.vis.grid(row=2,column=0,columnspan=5)
 
-
-
-		#vis.create_rectangle(50, 20, 150, 80, fill="#476042")
-		##w.create_rectangle(65, 35, 135, 65, fill="yellow")
-		#w.create_line(0, 0, 50, 20, fill="#476042", width=3)
-		#w.create_line(0, 100, 50, 80, fill="#476042", width=3)
-		#w.create_line(150,20, 200, 0, fill="#476042", width=3)
-		#w.create_line(150, 80, 200, 100, fill="#476042", width=3)
-
-		x0 = 0
-		y0 = 0
-		h = 30
-		w = 0
-
-		allTotal = 0
-		for t in self.tasks:
-			allTotal = allTotal + t.getTotalTime()
-
-		for t in self.tasks:
-
-			tot = t.getTotalTime()
-
-
-			for s in t.getSessions():
-
-				if(tot != 0):
-					w = (s.getTotalTime()*1.0 / allTotal)*150
-
-				print("\ns.tot: " + str(s.getTotalTime()))
-				print("w: " + str(w))
-				print("tot: " + str(tot))
-				vis.create_rectangle(x0, y0, x0+w, y0+h, fill="green")
-				x0 = x0+w
 
 			x0 = 0
-			y0 = y0 + h + 10
+			y0 = 0
+			h = 30
+			w = 0
 
-				#vis.create_rectangle(10,10,150,150,fill='black')
+			allTotal = 0
+			for t in self.tasks:
+				allTotal = allTotal + t.getTotalTime()
+
+			for t in self.tasks:
+
+				tot = t.getTotalTime()
+
+
+				for s in t.getSessions():
+
+					if(tot != 0):
+						w = (s.getTotalTime()*1.0 / allTotal)*150
+
+					print("\ns.tot: " + str(s.getTotalTime()))
+					print("w: " + str(w))
+					print("tot: " + str(tot))
+					self.vis.create_rectangle(x0, y0, x0+w, y0+h, fill="green")
+					x0 = x0+w
+
+				x0 = 0
+				y0 = y0 + h + 10
+			self.outputButton.config(relief=SUNKEN)
+		else:
+			self.visuVisible = FALSE
+			self.vis.grid_remove()
+			self.outputButton.config(relief=RAISED)
+
+
 
 
 
@@ -176,9 +183,6 @@ class GUI(Frame):
 				newTask = Task(ut)
 				self.tasks.append(newTask)
 
-
-
-
 		names = (o.name for o in self.tasks)
 		print(self.tasks)
 		self.updateTaskList()
@@ -197,61 +201,44 @@ class GUI(Frame):
 
 		self.parent.title("trak v0.1")
 
-		self.pack(fill=BOTH, expand=True)
+		self.pack(fill=BOTH, expand=False)
 		self.var = BooleanVar()
 
 		taskNames = []
 		for t in self.tasks:
 			taskNames.append(t.getName())
 
-
-
-
 		self.newTask = StringVar()
 		self.taskList = ttk.Combobox(self, textvariable=self.newTask, values=taskNames)
 
-		#self.taskList['values'] = taskNames
-		self.taskList.bind('<<ComboboxSelected>>',self.start)
-		#self.taskList.place(x='0', y='0')
-		#self.taskList.current(0)
-		self.taskList.grid(row=0,column=0)
-		#self.taskList.pack(side=LEFT)
+		self.taskList.bind('<<ComboboxSelected>>',self.start1)
 
-		print(self.newTask)
+
+		if len(self.tasks) > 0:
+			self.taskList.current(0)
+		self.taskList.grid(row=0,column=0)
 
 
 		self.playImg = PhotoImage(file='images/play.png')
 		self.startButton = Button(self, justify = LEFT, command = self.start, image=self.playImg)
-		#self.startButton.place(x=50, y=0)
-		#self.startButton.pack(side=LEFT)
 		self.startButton.grid(row=0,column=1)
 
 		self.pauseImg = PhotoImage(file='images/pause.png')
 		self.stopButton = Button(self, justify = LEFT, command = self.stop, image=self.pauseImg)
-		#self.stopButton.place(x=86, y=0)
-		#self.stopButton.pack(side=LEFT)
 		self.stopButton.grid(row=0,column=2)
 
 		self.ejectImg = PhotoImage(file='images/eject.png')
 		self.editButton = Button(self, justify = LEFT, command=self.editList, image=self.ejectImg)
-		#self.editButton.place(x=122, y=0)
-		#self.editButton.pack(side=LEFT)
 		self.editButton.grid(row=0,column=3)
 
 		self.outputImg = PhotoImage(file='images/menu-lines.png')
 		self.outputButton = Button(self, justify = LEFT, command=self.visualize, image=self.outputImg)
-		#self.editButton.place(x=122, y=0)
-		#self.editButton.pack(side=LEFT)
 		self.outputButton.grid(row=0,column=4)
 
 
 
-		#self.visuButton = Button(self, text="VISU", command = self.visualize)
-		#self.visuButton.place(x=150, y = 90)
-		
-		
-		#self.printButton = Button(self, text="PRINT", command = self.printAll)
-		#self.printButton.place(x=500	, y=300)
+
+
 
 		#self.newTask = IntVar()
 		#i = 0
@@ -261,45 +248,16 @@ class GUI(Frame):
 		#	i = i+1
 		        #t.getName()
 
-
-
-
 		#http://pyinmyeye.blogspot.fi/2012/08/tkinter-combobox-demo.html
-		#todo add to list
-		#cb1 = ttk.Combobox(self)
-		#cb1.bind('<Return>', self.updateList)
-		#cb1.pack(pady=5, padx=10)
-        
-		#Radiobutton(self, text="One", value=1).pack(anchor=W)
-		#Radiobutton(self, text="Two", value=2).pack(anchor=W)
 
 
-
-		#startButton = Button(self, text="START", command=self.start)
-		#startButton.place(x=50, y=50)
-
-		#cb = Checkbutton(self, text="Show title",
-		#    variable=self.var, command=self.onClick)
-		#cb.select()
-		#cb.place(x=50, y=50)
-
-
-
-	#def closeEvent(self):
-	#	print ("exit handler")
-	#	self.destroy()
 
 def doSomethingOnExit(tasks):
 	print ("atexit" + str(len(tasks)))
 	pickle.dump( tasks, open( "trak.p", "wb" ) )
 
 
-
-
 def main():
-
-
-
 
 	#tasks = []
 	#for i in range(0,5):
@@ -312,34 +270,19 @@ def main():
 
 	if os.path.exists('trak.p'):
 		tasks = pickle.load( open( "trak.p", "rb" ) )
-
-
 		app = GUI(root, tasks)
-		#root.geometry("250x150+300+300")
-		#app.loadTasks(tasks)
 
-		#root.protocol( "WM_DELETE_WINDOW", closeMe )
-
-
-
-	#init tasks
 	else:
 
 		tasks = []
 
 		app = GUI(root, tasks)
-		#app.loadTasks(tasks)
 		app.editList()
 
-	#print('loaded tasks: ')
-	#for t in loadedTasks:
-	#	print(t.getName())
 
 	atexit.register(doSomethingOnExit, tasks)
+	root.wm_attributes("-topmost", 1)
 	root.mainloop()
-
-
-
 
 
 if __name__ == '__main__':
